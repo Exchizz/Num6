a580426d2cf1479136c0c670557a5253229e9105
Mathias Neerup <manee12@student.sdu.dk>
2016-04-21 11:05:02 +0200

    Fixed error in second loop

--- PF2/main.c
+++ PF2/main.c
@@ -30,13 +30,21 @@ double F(double x, double y, double d_param){
 
 
 
-// Number of trapetz
-int n = 4;
 
-// numper of points
-int Npoints = n + 1;
+int main(int argc, char** argv) {
+	// Number of trapetz
+	int n = atoi(argv[1]);
+	if(argv[1] == ""){
+		std::cout << "default N = 4";
+		n=4;
+	}
 
-int main() {
+
+	std::cout << "n: " << n << std::endl;
+
+	// numper of points
+	int Npoints = n + 1;
+
 	double a = -0.5;
 	double b = 0.5;
 
@@ -75,12 +83,9 @@ int main() {
 		for(int col = Npoints; col < 2*Npoints; col++){
 			divisor = (y == a || y == b) ? 2 : 1;
 			std::cout << "divisor: " << divisor << std::endl;
-//			A[row][col] = divisor;
 			A[row][col] = -ALPHA2*(h/divisor)*F(x,y,d);
-//			cout << "x: " << x << " y:" << y << "\t" ;
 			y+=h;
 		}
-//		std::cout << endl;
 		y = a;
 		x+=h;
 	}
@@ -92,13 +97,10 @@ int main() {
 	for(int row = Npoints; row < 2*Npoints; row++){
 		for(int col = 0; col < Npoints; col++){
 			divisor = (x == a || x == b) ? 2 : 1;
-//			A[row][col] = divisor;
 			A[row][col] = -BETA2*(h/divisor)*F(x,y,d);
-//			cout << "x:" << x << " y:" << y << "\t" ;
 			x+=h;
 		}
 		x=a;
-//		std:cout << endl;
 		y+=h;
 	}
 
@@ -113,31 +115,32 @@ int main() {
 
 	cout << "X vector: " << endl;
 
-	ofstream myfile;
+	ofstream u_x_file;
+	ofstream v_y_file;
 
-	std::ostringstream ss;
-	ss << "results_" << n << ".csv";
+	u_x_file.open ( "results_u_x.csv", ios::app );
+	v_y_file.open ( "results_v_y.csv", ios::app );
+//	u_x_file << "N  &  -0.5    & -0.25   & 0       & 0.25    & 0.5     &  \\ \cline{1-6}" << std::endl;
 
-	myfile.open ( ss.str() );
-	myfile << "x:,";
+//	for(int i = 0; i < Npoints; i++){
+//		if(a+(i*h) == a || a+(i*h) == -0.25 || a+(i*h) == 0 || a+(i*h) == 0.25 || a+(i*h) == b )
+//			u_x_file << ".< & . & . & . & . & . &  \\ \cline{1-6}"";
+//	}
+//	u_x_file << endl;
 
+	u_x_file << n << " & ";
 	for(int i = 0; i < Npoints; i++){
 		if(a+(i*h) == a || a+(i*h) == -0.25 || a+(i*h) == 0 || a+(i*h) == 0.25 || a+(i*h) == b )
-			myfile << a+(i*h) << ",";
+			u_x_file << X[i] << " & ";
 	}
-	myfile << endl;
-
-	myfile << "u(x),";
+	u_x_file << "\\\\ \\cline{1-6}" << std::endl;
+	v_y_file << n << " & ";
 	for(int i = 0; i < Npoints; i++){
 		if(a+(i*h) == a || a+(i*h) == -0.25 || a+(i*h) == 0 || a+(i*h) == 0.25 || a+(i*h) == b )
-			myfile << X[i] << ",";
+			v_y_file << X[Npoints+i] << " & ";
 	}
-	myfile << endl;
-	myfile << "v(y),";
+	v_y_file << "\\\\ \\cline{1-6}" << std::endl;
 
-	for(int i = 0; i < Npoints; i++){
-		if(a+(i*h) == a || a+(i*h) == -0.25 || a+(i*h) == 0 || a+(i*h) == 0.25 || a+(i*h) == b )
-			myfile << X[Npoints+i] << ",";
-	}
-	myfile.close();
+	v_y_file.close();
+	u_x_file.close();
 }
